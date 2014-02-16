c358f50a97088547ae24aa3a01fdd3c3ed0090a2
Mat Silva <askmatsilva@gmail.com>
2014-02-16 18:30:49 -0500

    updated _X to return the item with _X.prototypes or just new _X if itemName is black

--- _X.js
+++ _X.js
@@ -1,18 +1,27 @@
+//Init _X
 function _X (itemName){
-    for (var i = 1; i <= app.project.numItems; i++) {
-                if (app.project.item(i).name == itemName) {
-                    return app.project.item(i);
-                }
-            }
-    alert("Item does not exist", "_X Error");
-    return _X;
+    if(!itemName) return new _X();
+    var item;
+    this.getItem = function(itemName){
+       for (var i = 1; i <= app.project.numItems; i++) {
+        if (app.project.item(i).name == itemName) {
+            return app.project.item(i);
+        }
+    }};
+    item = this.getItem(itemName);
+    item.prototype = new _X();
+    
+    return item;
 };
-//make sure to refer to this when callthing methods, then return this to make it chainable
+//make sure to refer to this when calling methods, then return this to make it chainable
 
 //For CompItems
 _X.prototype.getLayer = function (layerName){};
 _X.prototype.hasLayer = function (layerName){};
 _X.prototype.changeName = function(newName){};
+_X.prototype.alertName = function(){
+    alert(this.name);
+};
 
 //For FolderItems
 
@@ -21,3 +30,5 @@ _X.prototype.changeName = function(newName){};
 _X.prototype.assert = function (condition, msg) {
     alert(msg, (condition ? "pass" : "fail"));
 };
+
+
